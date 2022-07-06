c71f463e3d8eeb5818dd781d38801a7beb9c3667
guilhermeKuffner <68231357+guilhermeKuffner@users.noreply.github.com>
2022-07-06 14:47:56 -0300

    enunciados posicionados

--- EstruturaSequencial.py
+++ EstruturaSequencial.py
@@ -1,6 +1,12 @@
 from math import ceil
 
 #1° ARREDONDAR NUMEROS
+
+#João Papo-de-Pescador, homem de bem, comprou um microcomputador para controlar o rendimento diário de seu trabalho. Toda vez que ele traz um peso de peixes maior que o estabelecido pelo 
+#regulamento de pesca do estado de São Paulo (50 quilos) deve pagar uma multa de R$ 4,00 por quilo excedente. João precisa que você faça um programa que leia a variável peso (peso de peixes)
+#e calcule o excesso. Gravar na variável excesso a quantidade de quilos além do limite e na variável multa o valor da multa que João deverá pagar. Imprima os dados do programa com as mensagens
+#adequadas.
+
 print("peso")
 peso=float(input())
 result = peso - 50
@@ -9,6 +15,14 @@ print (result, oi)
 
 
 #2° PORCENTAGEM
+
+#Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês.
+#Calcule e mostre o total do seu salário no referido mês, sabendo-se que são descontados 11% para o Imposto de Renda, 8% para o INSS e 5% para o sindicato, faça um programa que nos dê:
+#salário bruto.
+#quanto pagou ao INSS.
+#quanto pagou ao sindicato.
+#quanto pagou ao IR.
+#o salário líquido.
 print("qual seu salario bruto?")
 salB=float(input())
 porc = salB/100
@@ -23,6 +37,10 @@ print("Imposto de Renda",ir)
 print(" salario liquido",liq)
 
 #3° Resto de divisão
+
+#Faça um programa para uma loja de tintas. O programa deverá pedir o tamanho em metros quadrados da área a ser pintada.
+# Considere que a cobertura da tinta é de 1 litro para cada 3 metros quadrados e que a tinta é vendida em latas de 18 litros,
+# que custam R$ 80,00. Informe ao usuário a quantidades de latas de tinta a serem compradas e o preço total.
 print("Qual o tamanho da area a ser pintada em metros quadrados?")
 area=int(input())
 lit = area/3
@@ -32,7 +50,15 @@ if(rest >0):
  lata=lata+1
 print("você precisará de", lata, "lata/s e o total á pagar é de:R$",lata*80)
 
-#4° Resto de divisão com arredondamento e posibilidades 
+#4° Resto de divisão com arredondamento e posibilidades
+
+#Faça um Programa para uma loja de tintas. O programa deverá pedir o tamanho em metros quadrados da área a ser pintada. 
+# Considere que a cobertura da tinta é de 1 litro para cada 6 metros quadrados e que a tinta é vendida em latas de 18 litros, que custam R$ 80,00 ou em galões de 3,6 litros, 
+# que custam R$ 25,00.Acrescente 10% de folga e sempre arredonde os valores para cima, isto é, considere latas cheias
+#Informe ao usuário as quantidades de tinta a serem compradas e os respectivos preços em 3 situações:
+#comprar apenas latas de 18 litros;
+#comprar apenas galões de 3,6 litros;
+#misturar latas e galões, de forma que o desperdício de tinta seja menor.
 print("Qual o tamanho da area a ser pintada em metros quadrados?")
 area=int(input())
 lit = area/6
